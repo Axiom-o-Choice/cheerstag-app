39fd56852b619371a5ca0108f184b250e7ca6787
Max Rietmeijer <max@bouwers.dev>
2025-09-23 18:16:09 +0200

    Changed home content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,25 +3,27 @@
         <img src="@/assets/images/logo.png" width="90%" style="margin-top: 60px" />
         <img src="@/assets/images/visual_mobile.png" width="60%" style="margin: 10px 0 40px 0" />
         <div class="content">
-            Hey ben je ook ge-tagged?
+            Hey ben je ook ge-tagged?<br />
+            Ben jij ook wel eens iets vergeten?
             <br /><br />
-
-            Maak ook van Cadeau naar een blijvende herinnering. Veel te vaak verliezen cadeaus hun emotionele waarde.
-            <br /><br />
-
+            Een cadeau krijgen is prachtig - maar de herinnering eraan vervaagt vaak sneller dan je denkt.
             <ul style="text-align: left">
-                <li>Vergeten herkomst: Die mooie fles wijn, maar van wie was die ook alweer?</li>
+                <li>Vergeten herkomst: Die mooie fles wijn... maar van wie was hij ook alweer?</li>
                 <li>
-                    Verloren context: Het cadeau is er nog, maar de reden en het moment van ontvangst zijn verdwenen
+                    Verloren context: Je ziet het cadeau nog, maar weet niet meer bij welke gelegenheid je het kreeg.
                 </li>
-                <li>Gemiste dankbaarheid: Geen idee wie je moet bedanken voor dat attente gebaar</li>
-                <li>Vluchtige impact: Zonder verhaal wordt elk cadeau gewoon een object</li>
+                <li>Gemiste dankbaarheid: Je wil bedanken bedanken, maar weet niet meer wie jou blij heeft gemaakt</li>
+                <li>Vluchtige impact: Zonder verhaal wordt elk cadeau uiteindelijk gewoon een object.</li>
             </ul>
-
-            Dus.. Waardevolle momenten van verbinding gaan voor altijd verloren. CheersTag koppelt een herinneringen aan
-            jouw fysieke cadeaus elk cadeau vertelt een verhaal - wij zorgen dat het verhaal nooit verloren gaat"
+            Zo verdwijnen waardevolle momenten van verbinding.
             <br /><br />
-
+            Met CheersTag maak je van een cadeau een blijvende herinnering.<br />
+            Wij koppelen verhalen en emoties aan fysieke cadeaus.
+            <br /><br />
+            Zo blijft niet alleen het cadeau, maar vooral het verhaal erachter, voor altijd bewaard.
+            <br /><br />
+            Elk cadeau vertelt een verhaal - met CheersTag vergeet je het nooit. Tag je mee?
+            <br /><br />
             Dit is een prototype, <a href="mailto:info@cheerstag.nl">we horen graag jouw ervaring</a>.
         </div>
     </div>
